4f2305a9367f49fce65889820e338ad51fdadd4c
Benideck Longakit <longakitbenideck@gmail.com>
2025-09-08 01:20:10 +0800

    [Feature][Longakit] Homepage and Navbar fix
    - Added basic ui for homepage
    - Added file directory for pages and settings in navbar
    - Added home and notification bell in navbar

---
+++ kursor/frontend/app/settings/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Navbar from "@/components/homepage-navbar";
+
+export default function SettingsPage() {
+
+  return (
+    <div className="min-h-screen bg-white">
+        {/* Navbar */}
+            <Navbar />
+        <h1> 
+            Settings Pageeee
+        </h1>
+    </ div>
+  );
+}
